5d5a03b80062caf6ad7bd0281c4cd5c4c6f585aa
Yasmine <yasminebadarou.yb@gmail.com>
2024-09-30 16:20:45 +0200

    Ajout de tous les fichiers v2

---
+++ js/index.js
@@ -0,0 +1,95 @@
+function unapparait () {
+    document.getElementById("un").classList.toggle("undisparition");
+}
+
+function undispparait () {
+    document.getElementById("un").classList.toggle("undisparition");
+}
+
+function deuxapparait() {
+    document.getElementById("deux").classList.toggle("deuxdisparition");
+}
+
+function deuxdispparait() {
+    document.getElementById("deux").classList.toggle("deuxdisparition");
+}
+
+function troisapparait() {
+    document.getElementById("trois").classList.toggle("troisdisparition");
+}
+
+function troisdispparait() {
+    document.getElementById("trois").classList.toggle("troisdisparition");
+}
+
+function quatreapparait() {
+    document.getElementById("quatre").classList.toggle("quatredisparition");
+}
+
+function quatredispparait() {
+    document.getElementById("quatre").classList.toggle("quatredisparition");
+}
+
+function cinqapparait() {
+    document.getElementById("cinq").classList.toggle("cinqdisparition");
+}
+
+function cinqdispparait() {
+    document.getElementById("cinq").classList.toggle("cinqdisparition");
+}
+
+function sixapparait() {
+    document.getElementById("six").classList.toggle("sixdisparition");
+}
+
+function sixdispparait() {
+    document.getElementById("six").classList.toggle("sixdisparition");
+}
+
+function septapparait() {
+    document.getElementById("sept").classList.toggle("septdisparition");
+}
+
+function septdispparait() {
+    document.getElementById("sept").classList.toggle("septdisparition");
+}
+
+function huitapparait() {
+    document.getElementById("huit").classList.toggle("huitdisparition");
+}
+
+function huitdispparait() {
+    document.getElementById("huit").classList.toggle("huitdisparition");
+}
+
+function neufapparait() {
+    document.getElementById("neuf").classList.toggle("neufdisparition");
+}
+
+function neufdispparait() {
+    document.getElementById("neuf").classList.toggle("neufdisparition");
+}
+
+function dixapparait() {
+    document.getElementById("dix").classList.toggle("dixdisparition");
+}
+
+function dixdispparait() {
+    document.getElementById("dix").classList.toggle("dixdisparition");
+}
+
+function onzeapparait() {
+    document.getElementById("onze").classList.toggle("onzedisparition");
+}
+
+function onzedispparait() {
+    document.getElementById("onze").classList.toggle("onzedisparition");
+}
+
+function douzeapparait() {
+    document.getElementById("douze").classList.toggle("douzedisparition");
+}
+
+function douzedispparait() {
+    document.getElementById("douze").classList.toggle("douzedisparition");
+}
